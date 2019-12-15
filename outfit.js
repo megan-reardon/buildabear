e5b51683daa55cde630a836bcd628bb3e36fd984
Madison Randle <madisonrandle25@gmail.com>
2019-12-14 17:19:44 -0700

    Create object instance for an outfit

---
+++ outfit.js
@@ -0,0 +1,16 @@
+class Outfit {
+  constructor(id, title, background) {
+    this.id = id;
+    this.title = title;
+    this.garments = [];
+    this.background = background;
+  }
+  addGarment() {
+
+  }
+  removeGarment() {
+
+  }
+}
+
+module.exports = Outfit;
