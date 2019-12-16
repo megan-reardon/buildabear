f9d612ae4eee50fdd4869efad5fc06d40eba7a4b
Madison Randle <madisonrandle25@gmail.com>
2019-12-16 13:43:11 -0700

    Create functions to toggle hidden class on garments

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ var closet = document.querySelector('.saved-outfit');
 var outfitName = document.querySelector('input');
 var saveButton = document.querySelector('#save-button');
 var outfits = [];
-var hats = document.querySelector('.hats');
-var clothes = document.querySelector('.clothes');
-var accessories = document.querySelector('.accessories');
-var backgrounds = document.querySelector('.backgrounds');
+var garmentOptions = document.querySelector('.garment-option-container');
+var hatSection = document.querySelector('.hats')
 
 saveButton.addEventListener('click', saveOutfit);
-hats.addEventListener('click', addHat);
-clothes.addEventListener('click', addClothes);
-accessories.addEventListener('click', addAccessories);
-backgrounds.addEventListener('click', addBackgrounds);
+garmentOptions.addEventListener('click', indicateActivateButton);
+hatSection.addEventListener('click', hatConditionals);
+
 
 function saveOutfit() {
   event.preventDefault();
@@ -25,56 +22,48 @@ function saveOutfit() {
 
 // Function to highlight button when clicked
 
-function addHat(event) {
-  if(event.target.innerText === 'Top hat') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Sun hat') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Bow') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Crown') {
-  event.target.classList.toggle('button-active');
-  }
-}
-
-function addClothes(event) {
-  if(event.target.innerText === 'T-shirt') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Dress') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Tuxedo') {
+function indicateActivateButton(event) {
+  if(event.target.classList.contains('button')) {
     event.target.classList.toggle('button-active');
   }
 }
 
-function addAccessories(event) {
-  if(event.target.innerText === 'Necklace') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Bowtie') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Watch') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Monocle') {
-  event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Earrings') {
-  event.target.classList.toggle('button-active');
+function hatConditionals() {
+  addTopHat(event);
+  addSunHat(event);
+  addBow(event);
+  addCrown(event);
+}
+
+function addTopHat(event) {
+  var topHat = document.querySelector('#top-hat');
+  if (event.target.innerText === 'Top hat') {
+    topHat.classList.toggle('hidden');
   }
 }
 
-function addBackgrounds(event) {
-  if(event.target.innerText === 'Blue') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Park') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Beach') {
-    event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Space') {
-  event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Yellow') {
-  event.target.classList.toggle('button-active');
-  } else if(event.target.innerText === 'Hearts') {
-  event.target.classList.toggle('button-active');
+function addSunHat(event) {
+  var sunHat = document.querySelector('#sun-hat');
+  if (event.target.innerText === 'Sun hat') {
+    sunHat.classList.toggle('hidden');
   }
 }
+
+function addBow(event) {
+  var bow = document.querySelector('#bow');
+  if (event.target.innerText === 'Bow') {
+    bow.classList.toggle('hidden');
+  }
+}
+
+function addCrown(event) {
+  var crown = document.querySelector('#crown');
+  if (event.target.innerText === 'Crown') {
+    crown.classList.toggle('hidden');
+  }
+}
+
+
+
 
 //
