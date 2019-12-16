7b441af79074dd1e134c11a13c7d903eeafe59e6
Megan Venetianer <megan.venetianer@gmail.com>
2019-12-16 09:52:23 -0700

    Added functionality to highlight button when clicked

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ var closet = document.querySelector('.saved-outfit');
 var outfitName = document.querySelector('input');
 var saveButton = document.querySelector('#save-button');
 var outfits = [];
+var hats = document.querySelector('.hats');
 
 saveButton.addEventListener('click', saveOutfit);
+hats.addEventListener('click', addHat);
 
 function saveOutfit() {
   event.preventDefault();
@@ -13,6 +15,14 @@ function saveOutfit() {
       <img class="close" src="./assets/close.svg" alt="close-icon">
     </div>
   `);
+}
+
+// Function to highlight button when clicked
+
+function addHat(event) {
+  if(event.target.innerText === 'Top hat') {
+    event.target.classList.add('button-active');
+  } 
 }
 
 //
